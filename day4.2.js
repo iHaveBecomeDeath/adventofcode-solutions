a83f2157763605bd22e1895030dae081fc8c09f3
Kalle Wallin <kalle.wallin@gmail.com>
2015-12-09 00:33:33 +0100

    day 5.1

---
+++ day4.2.js
@@ -0,0 +1,22 @@
+var CryptoJS = require("./crypto-js/crypto-js");
+var input = 'bgvyzdsv'; 
+var md5 = function(str){
+	return CryptoJS.MD5(str).toString();
+}
+var done = false;
+var number = 254574;
+var md5String = '';
+
+while(!done){
+	md5String = md5(input + number);
+	if (md5String.substring(0,4) === '0000'){
+		console.log('0000', md5String, number);
+	}
+	if (md5String.substr(0,6) === '000000'){
+		done = true;
+		console.log('done!');
+	}
+	// console.log(md5String, done, number, input);	
+	number++;
+}
+	console.log(md5String, done, number-1, input);	
