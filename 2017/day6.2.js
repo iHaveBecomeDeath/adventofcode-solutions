c93285e85c486ac5ebec8fcf5c56597d73dcc175
Kalle Wallin <kalle.wallin@gmail.com>
2017-12-06 15:10:03 +0100

    day 6.2

---
+++ 2017/day6.2.js
@@ -0,0 +1,29 @@
+var input = [2, 8, 8, 5, 4, 2, 3, 1, 5, 5, 1, 2, 15, 13, 5, 14];
+var seenInputs = {};
+var inputLength = input.length;
+var iterations = 0;
+
+while(true) {
+  var seenName = input.join(",");
+  if (seenInputs[seenName] !== undefined) {
+    console.log(`found ${seenName} again, at ${iterations}. Originally at ${seenInputs[seenName]}`);
+    console.log(`Distance: ~~~${iterations - seenInputs[seenName]} ~~~`);
+    break;
+  } else {
+    seenInputs[seenName] = iterations;
+    console.log(`found ${seenName} at ${iterations}`);
+  }
+
+  var highestValue = Math.max(...input);
+  var highestIndex = input.indexOf(highestValue);
+  input[highestIndex] = 0;
+
+  for (var i = 1; i <= highestValue; i++) {
+    var increment = i + highestIndex;
+    var currentIndex = increment >= inputLength ?
+      increment % inputLength :
+      increment ;
+    input[currentIndex]++;
+  }
+  iterations++;
+}
